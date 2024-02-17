bd09c0345b21736b1340538d1c958fcb460d12d6
Colby Birkhead <birkheadc@gmail.com>
2024-02-17 19:53:56 +0900

    fix mistake in japanese translation; change font

--- index.tsx
+++ index.tsx
@@ -9,9 +9,10 @@ import './src/styles/global.css';
 import tekoBold from './src/fonts/Teko/static/Teko-SemiBold.ttf';
 import teko from './src/fonts/Teko/static/Teko-Regular.ttf';
 import abel from './src/fonts/Abel/Abel-Regular.ttf';
-import notoSerifJp from './src/fonts/Noto_Serif_JP/NotoSerifJP-Regular.otf';
+// import notoSerifJp from './src/fonts/Noto_Serif_JP/NotoSerifJP-Regular.otf';
 import ibmBold from './src/fonts/IBM_Plex_Sans_JP/IBMPlexSansJP-SemiBold.ttf';
 import ibm from './src/fonts/IBM_Plex_Sans_JP/IBMPlexSansJP-Medium.ttf';
+import ibmReg from './src/fonts/IBM_Plex_Sans_JP/IBMPlexSansJP-Regular.ttf';
 
 import App from './src/app/App';
 import { Font } from '@react-pdf/renderer';
@@ -21,7 +22,7 @@ import { Font } from '@react-pdf/renderer';
 Font.register({ family: 'en_Primary', src: abel });
 Font.register({ family: 'en_Header', src: tekoBold });
 Font.register({ family: 'en_Header_2', src: teko });
-Font.register({ family: 'jp_Primary', src: notoSerifJp });
+Font.register({ family: 'jp_Primary', src: ibmReg });
 Font.register({ family: 'jp_Header', src: ibmBold });
 Font.register({ family: 'jp_Header_2', src: ibm });
 
